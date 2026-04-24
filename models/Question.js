6c872a378af38f9950c8e6dddafac254814e23ee
Arxeo <domik0025@gmail.com>
2026-04-24 17:53:13 +0300

    Add allowCustom field to Question model
    Added allowCustom field to question schema.

--- models/Question.js
+++ models/Question.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 const questionSchema = new mongoose.Schema({
     question: { type: String, required: true, unique: true},    // Data and for search and show
-    answers: [{ type: String, required: true }]                 // .append({myanswer: "Моя відповідь"});
+    answers: [{ type: String, required: true }],                // .append({myanswer: "Моя відповідь"});
+
+    allowCustom: { type: Boolean, default: false } // Allows users to add their own answers
 });
 
 module.exports = mongoose.model("Question", questionSchema);
